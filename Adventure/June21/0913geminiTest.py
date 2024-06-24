1554c91aee005826c1f0778a5d9d37d07043e563
KDahiya <kishan_dahiya@heartcore.co.jp>
2024-06-24 10:26:13 +0900

    Added Todays new

--- Adventure/June21/0913geminiTest.py
+++ Adventure/June21/0913geminiTest.py
@@ -6,8 +6,7 @@ genai.configure(api_key=os.environ['GENERATIVE_AI_API_KEY'])
 # Choose a model that's appropriate for your use case.
 model = genai.GenerativeModel('gemini-1.5-flash')
 
-prompt = "Who is Nicki Minaj?"
-
+prompt = "Who is Obama?"
 response = model.generate_content(prompt)
 
 print(response.text)

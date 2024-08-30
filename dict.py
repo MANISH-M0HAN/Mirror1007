0a2121ccda18b96babbdc77d7f7b4e93102d4599
22jen <jennifer_i@heartcore.co.jp>
2024-08-30 16:24:37 +0900

    removed the words from dict.py and added it to spellchecker.py

--- dict.py
+++ dict.py
@@ -30,21 +30,6 @@ common_english_words = {
     "whether", "which", "while", "who", "whom", "whose", "why", "will", "with", "within", "without", 
     "won't", "would", "yes", "yet", "you", "your", "yours", "yourself", "yourselves", "input", "read", "wake"
 
-    # Jennifer 
-    "nausea", "vomit", "hand", "excercise", "precaution", "cancer", "parts", "stroke", "pills",
-    "conditions", "imbalance", "cell", "cure", "growth", "friend", "organs", "breast", "sufficient",
-    "reduce", "stress", "lumps", "stage", "extremely", "ill", "wear", "make", "sure", "get", "virus", "sex",
-    "become", "massive", "patients", "required", "face", "tissue", "fat", "shortage", "blood", "eyes", "body",
-    "issue", "tube", "testing", "baby", "small", "genes", "high"                
-
-    # Jennifer - Intent words
-    "What", "Define", "Identify", "Describe", "Clarify", "Specify", "Detail", "Outline", "State", "Explain", "Determine", "Depict", 
-    "Summarize", "Designate", "Distinguish", "Symptoms", "Signs", "Indications", "Manifestations", "Warning", "Clues", "Evidence", 
-    "Redflags", "Markers", "Presentations",  "Outcomes", "Patterns", "Phenomena", "Traits", "Occurrences","Why", "Causes", "Reason", 
-    "Purpose", "Explain", "Justification", "Origin", "Motive", "Trigger", "Rationale", "Grounds", "Basis", "Excuse", "Source", "Factor", 
-    "How", "Method", "Means", "Procedure", "Steps", "Technique", "Process", "Way", "Approach", "Strategy", "System", "Manner", 
-    "Framework", "Form", "Mode", "Prevention", "Avoidance", "Safeguard", "Protection", "Mitigation", "Reduction", "Intervention", 
-    "Defense", "Deterrence", "Shielding", "Do"
     }
 
 # Load Words from Multiple Columns

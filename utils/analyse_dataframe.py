0d4919b62e2b5f3a8e870e0d9e14ce853b9d628d
Mohan-sharma-Venkatesh <mohan_venkatesh@heartcore.co.jp>
2024-09-02 10:36:05 +0900

    analyse dataframe code split complete

--- utils/analyse_dataframe.py
+++ utils/analyse_dataframe.py
@@ -1,10 +1,10 @@
 from sklearn.metrics.pairwise import cosine_similarity
-
-
+from utils import load_prerequisites 
+import logging
+from utils import process_user_input
 
 def find_best_context(query, threshold):
-
-    query_embedding = embedding_model.encode([query.lower()])
+    query_embedding = load_prerequisites.embedding_model.encode([query.lower()])
 
     best_match_score = 0
     best_max_match_score = 0
@@ -13,7 +13,7 @@ def find_best_context(query, threshold):
     best_match_type = None
     best_max_match_type = None
 
-    for index, item_embeddings in enumerate(db_embeddings):
+    for index, item_embeddings in enumerate(load_prerequisites.db_embeddings):
         trigger_score = cosine_similarity(query_embedding, item_embeddings['trigger_embedding'].reshape(1, -1)).flatten()[0]
         synonym_scores = [cosine_similarity(query_embedding, syn_emb.reshape(1, -1)).flatten()[0] for syn_emb in item_embeddings['synonyms_embeddings']]
         keyword_scores = [cosine_similarity(query_embedding, kw_emb.reshape(1, -1)).flatten()[0] for kw_emb in item_embeddings['keywords_embeddings']]
@@ -26,22 +26,22 @@ def find_best_context(query, threshold):
 
         if trigger_score >= 0.65 or max_synonym_score >= 0.65 or max_keyword_score >= 0.65:
             logging.info(
-                f"Strong direct match found. Query: '{query}', Trigger Score: {trigger_score:.4f}, "
-                f"Synonym Score: {max_synonym_score:.4f}, Keyword Score: {max_keyword_score:.4f} "
-                f"Response: {database[index]}"
+                f"Strong direct match found. Query: '{query}', Trigger Score: {trigger_score:.4f},"
+                f"Synonym Score: {max_synonym_score:.4f}, Keyword Score: {max_keyword_score:.4f},"
+                f"Response: {load_prerequisites.database[index]}"
             )
             best_max_match_score = max(trigger_score, max_synonym_score, max_keyword_score)
-            best_max_match_response = database[index]
+            best_max_match_response = load_prerequisites.database[index]
             best_max_match_type = "Max Match"
         
         if avg_score > best_match_score and trigger_score < 0.65 and max_synonym_score < 0.65 and max_keyword_score < 0.65:
             logging.info(
                 f"Strong direct match found. Query: '{query}', Trigger Score: {trigger_score:.4f}, "
                 f"Synonym Score: {max_synonym_score:.4f}, Keyword Score: {max_keyword_score:.4f} "
-                f"Response: {database[index]}"
+                f"Response: {load_prerequisites.database[index]}"
             )
             best_match_score = avg_score
-            best_match_response = database[index]
+            best_match_response = load_prerequisites.database[index]
             best_match_type = "Avg Max Match"
             
     if best_match_score >= threshold and best_max_match_score < best_match_score:
@@ -187,5 +187,6 @@ def match_columns(query, best_match_response):
     )
 
     return best_match_response.get(best_column_name, "")
+
 
 

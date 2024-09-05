2ed5bc122d2a64f7d99a74fe013fdb513a73215c
Mohan-sharma-Venkatesh <mohan_venkatesh@heartcore.co.jp>
2024-09-05 15:10:18 +0900

    fix: fix setup scripts for ubuntu and mac

--- bin/setup_ubuntu.sh
+++ bin/setup_ubuntu.sh
@@ -5,22 +5,15 @@ set -e
 # Update package list and install prerequisites
 sudo apt update
 sudo apt install -y software-properties-common
+sleep 1
 
-# Check if Python 3 is installed, if not, install it
-if ! command -v python3 &> /dev/null; then
-  sudo apt install -y python3 python3-pip
-  if ! command -v python3 &> /dev/null; then
-    echo "Python 3 installation failed. Please install Python 3 manually."
-    exit 1
-  fi
-  echo "Python 3 installed successfully."
-else
-  echo "Python 3 is already installed."
-fi
-
-REQUIREMENTS_FILE="./python_scripts/requirements.txt"
+sudo apt install python3
+sleep 1
+REQUIREMENTS_FILE="requirements.txt"
 
 # Install required Python packages
-python3 -m pip install --upgrade pip
-python3 -m pip install -r "$REQUIREMENTS_FILE"
+python3 -m pip install --break-system-packages -r "$REQUIREMENTS_FILE"
+
 echo "Required packages installed from $REQUIREMENTS_FILE"
+sleep 1
+

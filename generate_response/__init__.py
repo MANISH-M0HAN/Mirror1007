571cabeeded1f9a388fe4565798f9cfbd5cd3245
MANISH-M0HAN <manish_mohan@heartcore.co.jp>
2024-09-06 01:35:47 +0000

    incorporating in main 1st try

---
+++ generate_response/__init__.py
@@ -0,0 +1,2 @@
+from .custom import get_response 
+from utils import process_user_input, is_domain_relevant, default_messages
---
+++ utils/default_messages.py
@@ -0,0 +1,3 @@
+def generate_response_with_placeholder(prompt):
+    response = "This is a placeholder response generated for your question."
+    return response

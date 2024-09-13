46581997d7fe37faeff87228748a9790bd10bfc3
KDahiya <kishan_dahiya@heartcore.co.jp>
2024-09-13 17:48:10 +0900

    Commit Info 1. Updated variable names Please check them 2. Updated and fixed the issue with logging statements. Used to show in the Terminal for multi-lined

--- utils/analyse_dataframe.py
+++ utils/analyse_dataframe.py
@@ -17,13 +17,15 @@ def match_generator(query_words):
             yield load_prerequisites.database[index]
 
 def score_matches(query_embedding):
-    best_match_score = 0
-    best_max_match_score = 0
-    best_match_response = []
-    best_max_match_response = []
-    best_match_type = None
-    best_max_match_type = None
-
+    avg_match_score = 0
+    max_match_score = 0
+    avg_match_response = []
+    max_match_response = []
+    avg_match_count = 0
+    max_match_count = 0
+    avg_match_flag = False
+    max_match_flag = False
+    
     logging.info(f"2)Cosine Match with Avg Score or Max")
     for index, item_embeddings in enumerate(load_prerequisites.db_embeddings):
         trigger_score = cosine_similarity(
@@ -51,49 +53,52 @@ def score_matches(query_embedding):
             or max_keyword_score >= 0.65
         ):
             logging.info(
-                f"Strong direct match found for one of the features.\nTrigger Score: {trigger_score:.4f}, "
-                f"Synonym Score: {max_synonym_score:.4f}, Keyword Score: {max_keyword_score:.4f} "
+                f"Strong direct match found for one of the features."
+                f"Trigger Score: {trigger_score:.4f} Synonym Score: {max_synonym_score:.4f}, Keyword Score: {max_keyword_score:.4f}"
                 f"Response: {load_prerequisites.database[index]}"
             )
-            best_max_match_score = max(
-                trigger_score, max_synonym_score, max_keyword_score
-            )
-            best_max_match_response.append(load_prerequisites.database[index])
-            best_max_match_type = "Max Match"
+            max_match_score = max(trigger_score, max_synonym_score, max_keyword_score)
+            max_match_response.append(load_prerequisites.database[index])
+            max_match_count += 1
+            max_match_flag = True
 
         if (
-            avg_score > best_match_score
+            avg_score > avg_match_score
             and trigger_score < 0.65
             and max_synonym_score < 0.65
             and max_keyword_score < 0.65
         ):
             logging.info(
-                f"Strong Average match found. Avg Score: {avg_score:.4f}, "
-                f"\nTrigger Score: {trigger_score:.4f}, Synonym Score: {max_synonym_score:.4f}, "
+                f"Strong Average match found. Avg Score: {avg_score:.4f},"
+                f"Trigger Score: {trigger_score:.4f}, Synonym Score: {max_synonym_score:.4f},"
                 f"Keyword Score: {max_keyword_score:.4f} Response: {load_prerequisites.database[index]}"
             )
-            best_match_score = avg_score
-            best_match_response.append(load_prerequisites.database[index])
-            best_match_type = "Avg Max Match"
+            avg_match_score = avg_score
+            avg_match_response.append(load_prerequisites.database[index])
+            avg_match_count += 1
+            avg_match_flag = True
+            
+    return (avg_match_score, max_match_score, 
+            avg_match_response, max_match_response, 
+            avg_match_count, max_match_count, 
+            avg_match_flag, max_match_flag)
 
-    return best_match_score, best_max_match_score, best_match_response, best_max_match_response, best_match_type, best_max_match_type
-
-def evaluate_matches(best_match_score, best_max_match_score, best_match_response, best_max_match_response, best_match_type, best_max_match_type, threshold):
-    logging.info("Evaluating matches with the threshold : ",threshold)
-
-    if best_match_score >= threshold and best_max_match_score < best_match_score:
+def evaluate_matches(avg_match_score, max_match_score, avg_match_response, max_match_response, avg_match_count, max_match_count, avg_match_flag, max_match_flag, threshold):
+    if avg_match_score >= threshold and max_match_score < avg_match_score:
         logging.info(
-            f"Best Match Score: {best_match_score:.4f}, Best Match Response: '{best_match_response}', Match Type: {best_match_type}"
+            f"Avg Match Score: {avg_match_score:.4f}, Avg Match Response: '{avg_match_response}"
+            f"Match Count: {avg_match_count}, Avg Match: {avg_match_flag}"
         )
         print("This is from Average Match Response")
-        return best_match_response
+        return avg_match_response
 
-    elif best_max_match_score > best_match_score:
+    elif max_match_score > avg_match_score:
         logging.info(
-            f"Max Match Score: {best_max_match_score:.4f}, Best Match Response: '{best_max_match_response}', Match Type: {best_max_match_type}"
+            f"Max Match Score: {max_match_score:.4f}, Max Match Response: '{max_match_response}'" 
+            f"Match Count: {max_match_count}, Max Match: {max_match_flag}"
         )
         print("This is from Max Match Response")
-        return best_max_match_response
+        return max_match_response
 
     else:
         logging.warning(
@@ -109,17 +114,22 @@ def find_best_context(query, threshold):
     if matches:
         return matches
 
-    best_match_score, best_max_match_score, best_match_response, best_max_match_response, best_match_type, best_max_match_type = score_matches(query_embedding)
-    
+       # Score the matches
+    (avg_match_score, max_match_score, 
+    avg_match_response, max_match_response,
+    avg_match_count, max_match_count,
+    avg_match_flag, max_match_flag) = score_matches(query_embedding)
+     
     return evaluate_matches(
-        best_match_score, best_max_match_score, 
-        best_match_response, best_max_match_response, 
-        best_match_type, best_max_match_type, 
+        avg_match_score, max_match_score, 
+        avg_match_response, max_match_response, 
+        avg_match_count, max_match_count,
+        avg_match_flag, max_match_flag,
         threshold
     )
     
-def match_columns(query, best_match_response):
-    best_match_response_flag = 0
+def match_columns(query, matched_response):
+    ambiguous_query_flag = 0
     
     intent_words = {
             'What': ['what', 'defin', 'identif', 'describ', 'clarif', 'specif', 'detail', 'outlin', 'state', 'explain', 'determin', 'depict', 'summar', 'design', 'distinguish'], 
@@ -135,20 +145,20 @@ def match_columns(query, best_match_response):
         for keyword in keywords:
             keyword_lower = keyword.lower()
             position = query.find(keyword_lower)
-            if position != -1 and best_match_response.get(column):
-                snippet_start = max(0, position - 10)  # 10 characters before the match
-                snippet_end = min(len(query), position + len(keyword_lower) + 10)  # 10 characters after the match
+            if position != -1 and matched_response.get(column):
+                snippet_start = max(0, position - 5)  # 5 characters before the match
+                snippet_end = min(len(query), position + len(keyword_lower) + 5)  # 5 characters after the match
                 snippet = query[snippet_start:snippet_end]
                 logging.info(f"Match found for column: {column} with keyword: '{keyword}' at position {position}. Snippet: '{snippet}'")
-                matching_columns.append((position, best_match_response[column]))
+                matching_columns.append((position, matched_response[column]))
                 match_found = True  
                 break  
 
     if not match_found and intent_words:
         first_column = next(iter(intent_words)) 
-        if best_match_response.get(first_column):
-            default_response = best_match_response[first_column]
-            best_match_response_flag = 1
+        if matched_response.get(first_column):
+            default_response = matched_response[first_column]
+            ambiguous_query_flag = 1
             logging.info(f"No match found. Fetching default response : {default_response} from column :{first_column}.")
             matching_columns.append((0, default_response))
 
@@ -157,7 +167,7 @@ def match_columns(query, best_match_response):
     responses = [response for _, response in matching_columns]
 
     if responses:
-        return " ".join(responses), best_match_response_flag
+        return " ".join(responses), ambiguous_query_flag
 
     # query_embedding = load_prerequisites.embedding_model.encode([query])
     # column_scores = cosine_similarity(query_embedding,load_prerequisites.column_embeddings).flatten()
@@ -165,4 +175,4 @@ def match_columns(query, best_match_response):
     # best_column_index = column_scores.argmax()
     # best_column_name = load_prerequisites.column_names[best_column_index]
     # logging.info(f"Fallback to best column match: {best_column_name} with score {column_scores[best_column_index]:.4f}")
-    # return best_match_response.get(best_column_name, ""), best_match_response_flag
+    # return avg_match_response.get(best_column_name, ""), best_match_response_flag

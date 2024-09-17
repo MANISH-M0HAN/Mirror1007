5a1cccb88cbd9a419be206402e9959d53f2eb890
Mohan-sharma-Venkatesh <mohan_venkatesh@heartcore.co.jp>
2024-09-17 10:05:11 +0900

    refactor: change status to title case

--- utils/json_response.py
+++ utils/json_response.py
@@ -7,35 +7,35 @@ def unauthorized_user_error():
             "bot-type": ""
         },
         "message": "Unauthorised request. The API key provided is not valid.",
-        "status": "error"
+        "status": "Error"
     }
 
     return jsonify(unauthorized_error_json), 401
 
 def success_response(chatbot_response, bot_type= None):
     if bot_type== None:
-        bot_type = "custom"
+        bot_type = "Custom"
     success_response_json = {
         "data": {
             "chatbot-response": chatbot_response,
             "bot-type": bot_type
         },
         "message": "Successfully recieved response",
-        "status": "success"
+        "status": "Success"
     }
 
     return jsonify(success_response_json), 200
 
 def validation_error(message, bot_type= None):
     if bot_type== None:
-        bot_type = "custom"
+        bot_type = "Custom"
     validation_error_json = {
         "data": {
             "chatbot-response": message,
             "bot-type": bot_type
         },
         "message": "Validation error",
-        "status": "error"
+        "status": "Error"
     }
 
     return jsonify(validation_error_json), 422
@@ -46,8 +46,8 @@ def internal_server_error(exception):
             "chatbot-response": "",
             "bot-type": ""
         },
-        "message": exception,
-        "status": "error"
+        "message": "Internal server error",
+        "status": "Error"
     }
 
     return jsonify(internal_server_error_json), 500

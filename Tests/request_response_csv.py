86ea5748dddff58ee59a9aec37f4741e4694709c
22jen <jennifer_i@heartcore.co.jp>
2024-09-20 12:38:00 +0900

    resolved conflicts in PR#25 with latest changes from main

--- Tests/request_response_csv.py
+++ Tests/request_response_csv.py
@@ -1,11 +1,7 @@
 import csv
 import os
 
-<<<<<<< HEAD
 def request_response(user_input,corrected_input, response):
-=======
-def request_response(user_input, response):
->>>>>>> c80ff75 (PR for adding env-example and revision of Test_me.py along with minor improvements (#22))
     file_path = "Debug/request_response_stack.csv"
     file_exists = os.path.isfile(file_path)
     
@@ -13,12 +9,6 @@ def request_response(user_input, response):
         writer = csv.writer(file)
         
         if not file_exists:
-<<<<<<< HEAD
             writer.writerow(["User Input","Corrected Input", "Response"])
         
         writer.writerow([user_input, corrected_input, response])
-=======
-            writer.writerow(["User Input", "Response"])
-        
-        writer.writerow([user_input, response])
->>>>>>> c80ff75 (PR for adding env-example and revision of Test_me.py along with minor improvements (#22))
--- generate_response/__init__.py
+++ generate_response/__init__.py
@@ -1,6 +1,2 @@
 from .custom import get_response 
-<<<<<<< HEAD
 from utils import is_domain_relevant, default_messages
-=======
-from utils import  is_domain_relevant, default_messages
->>>>>>> 85fb1b2 (resolving conflicts and rebasing)
